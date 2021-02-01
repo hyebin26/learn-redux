12fe014c684a6c1f01664fe422e5df2621afbb97
(hyebin) <(sksp4334@naver.com)>
2021-02-01 17:18:25 +0900

    add redux modules

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import "./exercise";
+import rootReducer from "./modules";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
+console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-  
